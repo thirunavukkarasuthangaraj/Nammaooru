ba916efe8f99cbd40246eae35d78d103068d9e7a
thiru <thiru.t@mavens-i.com>
2026-01-07 01:23:23 +0530

    fix: Add HashMap import to OrderController

--- backend/src/main/java/com/shopmanagement/controller/OrderController.java
+++ backend/src/main/java/com/shopmanagement/controller/OrderController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
